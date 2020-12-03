e334d623ebe426e20e6156fc226529ff6faa45c9
harabisj <4542628-harabisj@users.noreply.gitlab.com>
2020-12-03 15:10:29 +0100

    Rename Form1 to MainForm

--- BasketballScoreboard_Server/MainForm.cs
+++ BasketballScoreboard_Server/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BasketballScoreboard_Server
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
