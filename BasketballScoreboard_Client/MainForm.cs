dfc84ac4d3ed066e59251eb53b51a8b54693c9a1
harabisj <50618887+harabisj@users.noreply.github.com>
2020-12-03 18:24:44 +0100

    Make testing Client

--- BasketballScoreboard_Client/MainForm.cs
+++ BasketballScoreboard_Client/MainForm.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimpleTcp;
 
 namespace BasketballScoreboard_Client
 {
     public partial class MainForm : Form
     {
+        private SimpleTcpClient client;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Send("Test");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient("127.0.0.1", 23190);
+            client.Connect();
         }
     }
 }
